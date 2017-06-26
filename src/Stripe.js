f0c1e0aa369f991c1e566035d1c8cc380452c3ef
Alexander Stokes <stokesbga@gmail.com>
2017-06-26 15:35:16 -0400

    remove all private token operations
    add full support for createToken

--- src/Stripe.js
+++ src/Stripe.js
@@ -5,11 +5,6 @@ const STRIPE_URL = 'https://api.stripe.com/v1/';
 
 
 class Stripe {
-  stripeSecretKey: string;
-
-  constructor(apiKey: string) {
-    this.stripeSecretKey = apiKey;
-  }
 
   /**
    * Return the default header entries : Accept and Authorization
@@ -44,123 +39,34 @@ class Stripe {
     return result.json();
   }
 
-  /**
-   * Generic method to request Stripe
-   * @param id : the ID of object needed
-   * @param resource : Rest API ressource ie. tokens, charges, etc.
-   */
-  async stripeGetRequest(resource: string, id: string): Promise {
-    const result = await fetch(`${STRIPE_URL}${resource}/${id}`, {
-      method: 'GET',
-      headers: this.defaultHeader(),
-    });
-
-    return result.json();
-  }
 
   /**
-   * Generic method to delete resourse
-   * @param resource : Rest API ressource ie. tokens, charges, etc.
+   * Only operation allowed from client/Using only public token
+   * @param info : { number, exp_month, exp_year, address_city, address_country, address_line1,
+   * ... address_line2, address_state, address_zip, currency, cvc }
    */
-  async stripeDeleteRequest(resource: string): Promise {
-    const result = await fetch(`${STRIPE_URL}${resource}`, {
-      method: 'DELETE',
-      headers: this.defaultHeader(),
-    });
+  createToken(info): Promise {
+    let { number,
+      exp_month,
+      exp_year,
+      cvc,
+      address_city,
+      address_state,
+      address_country,
+      address_line1,
+      address_line2,
+      address_zip,
+      currency
+    } = info;
 
-    return result.json();
-  }
-
-  createToken(cardNumber: string, expMonth: string, expYear: string, cvc: string): Promise {
-    if (!cardNumber) throw new Error(`cardNumber${REQM}`);
-    if (!expMonth) throw new Error(`expMonth${REQM}`);
-    if (!expYear) throw new Error(`expYear${REQM}`);
+    if (!number) throw new Error(`cardNumber${REQM}`);
+    if (!exp_month) throw new Error(`expMonth${REQM}`);
+    if (!exp_year) throw new Error(`expYear${REQM}`);
     if (!cvc) throw new Error(`cvc${REQM}`);
 
     return this.stripePostRequest('tokens', {
-      'card[number]': cardNumber,
-      'card[exp_month]': expMonth,
-      'card[exp_year]': expYear,
-      'card[cvc]': cvc,
+      card: info
     });
-  }
-
-  createCustomer(token: string, email: string): Promise {
-    if (!token) throw new Error(`token${REQM}`);
-    if (!email) throw new Error(`email${REQM}`);
-
-    return this.stripePostRequest('customers', {
-      source: token,
-      email,
-      description: `Customer for email: ${email}`,
-    });
-  }
-
-  updateCustomer(customerId: string, properties: Object): Promise {
-    if (!customerId) throw new Error(`customerId${REQM}`);
-    if (!properties) throw new Error(`properties${REQM}`);
-
-    return this.stripePostRequest(`customers/${customerId}`, properties);
-  }
-
-  createCharge(amount: number, customer: string, description: string,
-    currency: string = 'eur'): Promise {
-    if (!amount && amount !== 0) throw new Error(`amount${REQM}`);
-    if (!customer) throw new Error(`customer${REQM}`);
-    if (!description) throw new Error(`description${REQM}`);
-
-    return this.stripePostRequest('charges', {
-      amount,
-      currency,
-      customer,
-      description,
-    });
-  }
-
-  refundCharge(chargeId: string): Promise {
-    if (!chargeId) throw new Error(`chargeId${REQM}`);
-
-    return this.stripePostRequest('refunds', {
-      charge: chargeId,
-    });
-  }
-
-  getCustomer(customerId: string): Promise {
-    if (!customerId) throw new Error(`customerId${REQM}`);
-
-    return this.stripeGetRequest('customers', customerId);
-  }
-
-  addCardToCustomer(token: string, customerId: string): Promise {
-    if (!token) throw new Error(`token${REQM}`);
-    if (!customerId) throw new Error(`customerId${REQM}`);
-
-    return this.stripePostRequest(`customers/${customerId}/sources`, {
-      source: token,
-    });
-  }
-
-  destroyCardOfCustomer(cardId: string, customerId: string): Promise {
-    if (!cardId) throw new Error(`cardId${REQM}`);
-    if (!customerId) throw new Error(`customerId${REQM}`);
-
-    return this.stripeDeleteRequest(`customers/${customerId}/sources/${cardId}`);
-  }
-
-  createSubscription(customerId: string, planId: string): Promise {
-    if (!customerId) throw new Error(`customerId${REQM}`);
-    if (!planId) throw new Error(`planId${REQM}`);
-
-    return this.stripePostRequest('subscriptions', {
-      customer: customerId,
-      plan: planId,
-    });
-  }
-
-  retrieveSubscription(subscriptionId: string): Promise {
-    if (!subscriptionId) throw new Error(`subscriptionId${REQM}`);
-
-    return this.stripePostRequest(`subscriptions/${subscriptionId}`, {});
   }
 }
 
